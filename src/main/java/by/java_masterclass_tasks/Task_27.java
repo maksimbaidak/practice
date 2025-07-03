ae32a6cad176dbe34864a43ad4bcf0cac560f022
baidakm <maksimbaidak@gmail.com>
2025-07-03 15:41:31 +0300

    refactor printSquare method

--- src/main/java/by/java_masterclass_tasks/Task_27.java
+++ src/main/java/by/java_masterclass_tasks/Task_27.java
@@ -2,26 +2,21 @@ package by.java_masterclass_tasks;
 
 public class Task_27 {
 
-    public static void printSquareStar(int num){
+    public static void printSquareStar(int num) {
         int lineA = 2;
         int lineB = num - 1;
 
-        for(int z = 1; z <= num; z++){
-            for (int v = 1; v <= num; v++){
+        for (int z = 1; z <= num; z++) {
+            for (int v = 1; v <= num; v++) {
 
-                if(z == num | z == 1){
+                if (z == num | z == 1) {
                     System.out.print("*");
                 } else {
 
-                    if(v == num | v == 1) {
+                    if (v == num | v == 1) {
                         System.out.print("*");
                     } else {
-
-                        if(lineA == v | lineB == v){
-                            System.out.print("*");
-                        } else {
-                            System.out.print(" ");
-                        }
+                        System.out.print((lineA == v | lineB == v) ? "*" : " ");
                     }
                 }
             }
